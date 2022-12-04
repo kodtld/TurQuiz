df555f19b97e7c9de5416bba56af2becfea235c2
kodtld <kodu@live.fi>
2022-12-04 22:59:23 +0200

    update Readme

---
+++ schema.sql
@@ -0,0 +1,30 @@
+CREATE TABLE Users(
+    id SERIAL PRIMARY KEY,
+    username TEXT UNIQUE,
+    email TEXT UNIQUE,
+    password TEXT
+);
+
+CREATE TABLE Quizs(
+    id SERIAL PRIMARY KEY,
+    subject TEXT UNIQUE,
+    private BOOLEAN,
+    creator_id INTEGER
+);
+
+CREATE TABLE Questions(
+    id SERIAL PRIMARY KEY,
+    question TEXT,
+    answer TEXT,
+    option_1 TEXT,
+    option_2 TEXT,
+    option_3 TEXT,
+    quiz_id INTEGER
+);
+
+CREATE TABLE Highscores(
+    id SERIAL PRIMARY KEY,
+    quiz_id INTEGER,
+    user_id INTEGER,
+    score INTEGER
+);
