0ffb2b5bd080ae84c7ad6f3b9f927a55872f94a2
kodtld <kodu@live.fi>
2022-12-16 02:12:35 +0200

    Altered rank colors

--- models/color.py
+++ models/color.py
@@ -15,11 +15,11 @@ title_colors = [Color("Dark Cyan","#2A8D88","dark"), Color("Medium Turquoise","#
 
 rank_colors = [
         Color("Bronze","#6E3D02","light"),
-        Color("Silver", "#303030", "light"),
+        Color("Silver", "#737373", "light"),
         Color("Gold", "#B79101","light"),
-        Color("Platinum", "#094A73", "light"),
+        Color("Platinum", "#548DB1", "light"),
         Color("Diamond", "#0F347A", "light"),
         Color("Champion", "#5C30A3","light"),
         Color("Grand Champion", "#910725", "light"),
-        Color("Complete", "#3F003D", "light")
+        Color("Complete", "#8A0986", "light")
 ]
